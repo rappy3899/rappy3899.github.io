43fa6831347d15ff3f620334b2be4a70e28ffdad
ruin.3899 <ruin.38999@gmail.com>
2026-03-27 19:47:41 +0900

    Restore quiz site link to sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,12 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://rappy3899.github.io', // 新ドメイン
-  integrations: [sitemap()],
+  site: 'https://rappy3899.github.io',
+  integrations: [
+    sitemap({
+      customPages: [
+        'https://rappy3899.github.io/touhouquiz/', // クイズサイトのURLを復活
+      ],
+    }),
+  ],
 });
